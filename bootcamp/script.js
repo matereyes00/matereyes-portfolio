05436c8d4734b6823f1bfceecd444b980b744a31
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-08-07 14:48:58 +0800

    Added a hamburger feature to the nav bar menu. Added js to accommodate this Connected Font Awesome to the head so that i can have the hamburger icon Adjusted CSS features immediately to hide and show it

--- bootcamp/script.js
+++ bootcamp/script.js
@@ -8,3 +8,11 @@ function changeElement() {
         changedElement.textContent = "Now you don't";
     }
   }
+
+/* =========== HAMBURGER MENU ============ */
+const hamburger = document.getElementById('hamburger');
+const navUL =  document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
