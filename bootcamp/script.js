829d3991468b4641635f301bd5dca5fab9893e21
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-08-09 15:56:08 +0800

    Added a section where you can count (poll) for airline food. Added a picture from flaticon. Changed CSS and JS accordingly.

--- bootcamp/script.js
+++ bootcamp/script.js
@@ -16,3 +16,29 @@ const navUL =  document.getElementById('nav-ul');
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 });
+
+/*========== POLL =============== */
+let chickenCount = document.getElementById("chicken-number-display");
+let fishCount = document.getElementById("fish-number-display");
+
+let count = 0;
+
+function addChicken() {
+  count += 1
+  chickenCount.textContent = count;
+}
+
+function addFish() {
+  count += 1
+  fishCount.textContent = count;
+}
+
+function removeChicken() {
+  count -= 1;
+  chickenCount.textContent = count;
+}
+
+function removeFish() {
+  count -= 1;
+  fishCount.textContent = count;
+}
