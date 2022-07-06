7803085ca5c5d3a68ff1a01db812a79f3cf68e6f
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-07-06 11:52:42 +0800

    Moved the javascript script tag to the bottom of the body. Made the numbers and operands appear in the screen.

--- PersonalBlog/Project_calculator/revised_calculator/calc.js
+++ PersonalBlog/Project_calculator/revised_calculator/calc.js
@@ -1,11 +1,22 @@
-let calculator_button = document.getElementsByClassName("calc-button");
-
-function change_text(){
-    let screen_content = document.getElementById("screen_content");
-    screen_content.textContent += "1";
-}
+const
+    buttons = document.getElementsByClassName("calc_button"),
+    screen_content = document.getElementById('screen_content');
 
 function clear_text() {
-    let screen_content = document.getElementById("screen_content");
-    screen_content.textContent = "0";
+    screen_content.textContent = '';
 }
+
+function backspace() {
+    screen_content.textContent -= screen_content.textContent;
+}
+
+
+// https://stackoverflow.com/questions/63199551/how-to-get-the-values-of-buttons-with-eventlistener
+Array.from(buttons).forEach(button =>
+    button.addEventListener("click", showNumber));
+
+function showNumber(event) { // Listener can access its triggering event
+    const button = event.target; // event's `target` property is useful
+    screen_content.innerHTML += button.value;
+}
+
