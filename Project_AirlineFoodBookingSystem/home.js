56de01fd086d2fa8d75008d1f17b7de0c4cd87b9
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-08-09 01:02:53 +0800

    User has to log-in before ordering. Made buttons disabled. Resets on refresh. Removed html contents inside transaction area div. Added a sources.txt file to take note of progress.

--- Project_AirlineFoodBookingSystem/home.js
+++ Project_AirlineFoodBookingSystem/home.js
@@ -12,20 +12,43 @@ let checkForStocks = document.getElementById("check-for-stocks");
 let validityMessage = document.getElementById("validity-message");
 
 let websiteServerActive = true;
+let transactionArea = document.getElementById("transaction-area");
+
+// checks if user is logged in
+function checkValidity() {
+    if (user.value === "") {
+        welcomeContainer.innerHTML += `
+            <p>Please input a valid username.</p>
+        `;
+    } else {
+        chickenBtn.disabled = false; //button is enabled
+        beefBtn.disabled = false; //button is enabled
+        fishBtn.disabled = false; //button is enabled
+    
+        welcomeContainer.innerHTML = `
+            <h1> Welcome to Dodo Airlines Food Booking page, ${user.value}! </h1>
+        `; 
+        renderTransactionArea();  
+    }
+}
+
+function renderTransactionArea() {
+    transactionArea.innerHTML = `
+        <h2> What would you like to order today, ${user.value}? </h2>
+        <h3> Transaction Area </h3>
+    `;
+}
 
 // log-in feature
 submitUserBtn.addEventListener("click", function() {
-    welcomeContainer.innerHTML = `
-    <h1> Welcome to Dodo Airlines Food Booking page, ${user.value}! </h1>
-    <h2> What would you like to order today, ${user.value}? </h2>`;
+    checkValidity();
 });
-
 
 const menu = {
     Food : ["Fish", "Chicken", "Beef"],
     Quantity : [30, 20, 12]
 }
-// create a promise
+// create a promise for customer to place an order
 let orderPromise = (time, work) => {
     return new Promise((resolve, reject) => {
         if (websiteServerActive) {
@@ -38,12 +61,11 @@ let orderPromise = (time, work) => {
     });
 };
 
-
 chickenBtn.addEventListener("click", function() {
     // NO STOCKS AVAILABLE
     if (menu.Quantity[1] === 0) {
         setTimeout( () => {
-            checkForStocks.innerHTML = `No stocks available`
+            transactionArea.innerHTML += `<p>No stocks available</p>`
         }, 2000);
     } 
     // STOCKS AVAILABLE
@@ -53,37 +75,37 @@ chickenBtn.addEventListener("click", function() {
         
         .then( ()=> {
             return orderPromise(1000, () => {
-                checkForStocks.innerHTML = `Checking for stocks...`
+                transactionArea.innerHTML += `<p>Checking for stocks...</p>`
                 setTimeout( () => {
-                    checkForStocks.innerHTML += "We have stocks!"
+                    transactionArea.innerHTML += "We have stocks!"
                 }, 1000);
             })
         })
         
         .then( () => {
             return orderPromise(2000, () => {
-                orderMessage.innerHTML = `${user.value} ordered ${menu.Food[1]}.`
+                transactionArea.innerHTML += `<p>${user.value} ordered ${menu.Food[1]}.</p>`
             })
         })
 
         .then( () => {
             return orderPromise(1000, () => {
-                validityMessage.innerHTML = `${user.value}'s order of ${menu.Food[1]} has been confirmed.`
+                transactionArea.innerHTML += `<p>${user.value}'s order of ${menu.Food[1]} has been confirmed.</p>`
             })
         })
         
         .catch( () => console.log("Website not active. Cannot place order."))
         
-        .finally( () => validityMessage.innerHTML += "Thank you for shopping with us")
+        .finally( () => transactionArea.innerHTML += "<p>Thank you for shopping with us!</p>")
     }
 
     
 });
 
 fishBtn.addEventListener("click", function() {
-    orderMessage.innerHTML += `You ordered ${menu.Food[0]}`
+    transactionArea.innerHTML += `You ordered ${menu.Food[0]}`
 });
 
 beefBtn.addEventListener("click", function() {
-    orderMessage.innerHTML += `You ordered ${menu.Food[2]}`
+    transactionArea.innerHTML += `You ordered ${menu.Food[2]}`
 });
