7486b0e99a475a4b46ddf149904d4a102ff2c59d
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2022-12-17 22:59:00 +0800

    Cleaned the toggle for stacks. (Made it look like an arrow)

--- PersonalBlog/script.js
+++ PersonalBlog/script.js
@@ -18,28 +18,40 @@ const languages_panel = document.getElementById('languages-panel');
 const tools_panel = document.getElementById('tools-panel');
 const frameworks_panel = document.getElementById('frameworks-panel');
 
-let arrow_toggle = document.getElementsByClassName("arrow");
+let arrow_toggle1 = document.querySelector('.toggle1');
+let arrow_toggle2 = document.querySelector('.toggle2');
+let arrow_toggle3 = document.querySelector('.toggle3');
+
 
 ShowLangBtn.addEventListener("click", function() {
   if (languages_panel.style.display === "none") {
     languages_panel.style.display = "block";
+    arrow_toggle1.style.transform = 'rotate(0deg)';
+    // arrow_toggle.forEach(el => el.style.transform = 'rotate(0deg)');
   } else {
     languages_panel.style.display = "none";
+    arrow_toggle1.style.transform = 'rotate(180deg)';
+    // arrow_toggle.forEach(el => el.style.transform = 'rotate(180deg)');
+    // https://stackoverflow.com/questions/60917073/how-can-i-select-the-all-classes-in-javascript-with-same-classname
   }
 });
 
 ShowToolsBtn.addEventListener("click", function() {
   if (tools_panel.style.display === "none") {
     tools_panel.style.display = "block";
+    arrow_toggle2.style.transform = 'rotate(0deg)';
   } else {
     tools_panel.style.display = "none";
+    arrow_toggle2.style.transform = 'rotate(180deg)';
   }
 });
 
 ShowFrameworksBtn.addEventListener("click", function() {
   if (frameworks_panel.style.display === "none") {
     frameworks_panel.style.display = "block";
+    arrow_toggle3.style.transform = 'rotate(0deg)';
   } else {
     frameworks_panel.style.display = "none";
+    arrow_toggle3.style.transform = 'rotate(180deg)';
   }
 });
