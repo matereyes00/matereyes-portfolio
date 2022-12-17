e1d61d041c61553055fb978b06a800fa977344da
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-12-17 12:24:29 +0800

    Created a toggle for tools,languages and frameworks stack. Attempting to toggle the picture in about page

--- PersonalBlog/script.js
+++ PersonalBlog/script.js
@@ -1,47 +1,43 @@
-let button = document.getElementById("changing-button");
-let changedElement = document.getElementById("changed-element");
-
-function changeElement() {
-    if (changedElement.textContent === "Now you don't") {
-      changedElement.textContent = "Now you see me!";
-    } else {
-        changedElement.textContent = "Now you don't";
-    }
-  }
-
-// == Plays audio ==
-// let song = document.getElementById("song");
-// song.play();
-
-/*========== POLL =============== */
-let chickenCount = document.getElementById("chicken-number-display");
-let fishCount = document.getElementById("fish-number-display");
-
-let count_chckens = 0;
-let count_fish = 0;
-
-function addChicken() {
-  count_chckens += 1
-  chickenCount.textContent = count_chckens;
-}
-
-function addFish() {
-  count_fish += 1
-  fishCount.textContent = count_fish;
-}
-
-function removeChicken() {
-  count_chckens -= 1;
-  chickenCount.textContent = count_chckens;
-}
-
-function removeFish() {
-  count_fish -= 1;
-  fishCount.textContent = count_fish;
-}
-
 //  ===========
-let aboutProfilePic = document.getElementsById("profilepic");
-function changePic() {
-  document.getElementById("profilepic").src="images/mate.jpg";
-}
+let aboutProfilePic = document.getElementById("profilepic");
+aboutProfilePic.addEventListener("click", function() {
+  
+  if (aboutProfilePic.src = "images/decentmate.jpg") {
+    document.getElementById("profilepic").src="images/mate.jpg";
+  } else {
+    document.getElementById("profilepic").src="images/decentmate.jpg";
+  }
+  
+});
+
+// toggle between hiding/showing: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+const ShowLangBtn = document.getElementById("show-languages");
+const ShowToolsBtn = document.getElementById('show-tools');
+const ShowFrameworksBtn = document.getElementById('show-frameworks');
+const languages_panel = document.getElementById('languages-panel');
+const tools_panel = document.getElementById('tools-panel');
+const frameworks_panel = document.getElementById('frameworks-panel');
+
+ShowLangBtn.addEventListener("click", function() {
+  if (languages_panel.style.display === "none") {
+    languages_panel.style.display = "block";
+  } else {
+    languages_panel.style.display = "none";
+  }
+});
+
+ShowToolsBtn.addEventListener("click", function() {
+  if (tools_panel.style.display === "none") {
+    tools_panel.style.display = "block";
+  } else {
+    tools_panel.style.display = "none";
+  }
+});
+
+ShowFrameworksBtn.addEventListener("click", function() {
+  if (frameworks_panel.style.display === "none") {
+    frameworks_panel.style.display = "block";
+  } else {
+    frameworks_panel.style.display = "none";
+  }
+});
