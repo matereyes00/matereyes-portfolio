c0bbcedf6d2b707dafb1d21e270333cb605ff425
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2025-01-20 21:55:13 +0800

    removed version 1 of website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import About from './About_/About'; // Import the About component
@@ -13,10 +12,11 @@ function App() {
       <div className="App">
         <Header />
         <main>
-          {/* Use Routes instead of Switch, and element instead of component */}
           <Routes>
+            {/* Home Page */}
             <Route path="/" element={<h2>Home Page</h2>} />
-            <Route path="/about" element={<About />} /> {/* Link to About page */}
+            {/* About Page */}
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
         <Footer />
