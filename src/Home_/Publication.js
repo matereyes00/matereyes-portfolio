4857bb278e84923d58fdc8aa4513fc016914f3c1
Matê Reyes <martina.therese.reyes@student.ateneo.edu>
2025-01-30 21:06:53 +0800

    Added a video embed for the Publications page. Mode the who am i a separate component from event.  Shortened code in Home.js

--- src/Home_/Publication.js
+++ src/Home_/Publication.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Home.css';
 
-const Publication = ({ PubTitle, PubId, PubDescription,articleUrl, PubDate, PubAuthors }) => {
+const Publication = ({ PubTitle, PubId, PubDescription,articleUrl, PubDate, PubAuthors, embed }) => {
     
     return (
         <div id={PubId}>
             <h1><a href={articleUrl} target="_blank" rel="noreferrer">{PubTitle}</a></h1>
             <h4>{PubDate} | {PubAuthors}</h4>
             <p>{PubDescription}</p>
+            {embed}
         </div>
     );
 };
