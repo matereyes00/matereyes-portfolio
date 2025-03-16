5e6df6291e6c951de0a074d3235f4044c3841b71
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2025-03-16 18:12:28 +0800

    Made stack its own route.
    Added images of myself and for the stack.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-
 import Header from './Header';
 import Footer from './Footer';
 import About from './About_/About';
+import Stack from './Stack_/Stack';
 import Projects from './Projects_/Projects'; 
 import Home from './Home_/Home'; 
 import './App.css';
@@ -20,6 +21,7 @@ function App() {
               <Route path="/" element={<Home />} />
               {/* About Page */}
               <Route path="/about" element={<About />} />
+              <Route path="/stack" element={<Stack />} />
               {/* Catch-all route: Redirect unknown routes to Home */}
               <Route path="/projects" element={<Projects />} />
               <Route path="*" element={<Navigate to="/" replace />} />
