d5ac5176cd8fde6b0a007c9b36da37f4ff8a8ea5
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2025-03-19 20:53:54 +0800

    Updated stack page of the website

--- src/Stack_/Stack.js
+++ src/Stack_/Stack.js
@@ -1,68 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Select from "react-dropdown-select";
 
-import StackItem from './StackItem';
+import StackItem from "./StackItem";
 
-
-import react_logo from '../images/logo.svg';
-import python_logo from '../images/python.svg';
-import html from '../images/html.svg';
-import css from '../images/css.svg';
-import js from '../images/javascript.svg';
-import android_studio from '../images/android-studio.svg';
-import django from '../images/django.svg';
-import jupyter from '../images/jupyter.svg';
-import streamlit from '../images/streamlit.svg';
-import tableau from '../images/tableau-software.svg';
-import pytorch from '../images/pytorch.svg';
-import postgres from '../images/postgresql.svg';
-import quasar from '../images/quasar.svg';
-import figma from '../images/figma.svg';
-import qgis from '../images/qgis.svg';
-import git from '../images/git.svg';
-import github from '../images/github.svg';
-import docker_logo from '../images/docker-svgrepo-com.svg'
-import postman_logo from '../images/postman-icon-svgrepo-com.svg'
-import tailwind_logo from '../images/tailwind-svgrepo-com.svg'
+import react_logo from "../images/logo.svg";
+import python_logo from "../images/python.svg";
+import html from "../images/html.svg";
+import css from "../images/css.svg";
+import js from "../images/javascript.svg";
+import android_studio from "../images/android-studio.svg";
+import django from "../images/django.svg";
+import jupyter from "../images/jupyter.svg";
+import streamlit from "../images/streamlit.svg";
+import tableau from "../images/tableau-software.svg";
+import pytorch from "../images/pytorch.svg";
+import postgres from "../images/postgresql.svg";
+import quasar from "../images/quasar.svg";
+import figma from "../images/figma.svg";
+import qgis from "../images/qgis.svg";
+import git from "../images/git.svg";
+import github from "../images/github.svg";
+import docker_logo from "../images/docker-svgrepo-com.svg";
+import postman_logo from "../images/postman-icon-svgrepo-com.svg";
+import tailwind_logo from "../images/tailwind-svgrepo-com.svg";
 
 // docker
 // postman
 
-
 const AboutStack = () => {
-    const images = {
-        react_logo: { src: react_logo, name: "React" },
-        python_logo: { src: python_logo, name: "Python" },
-        html: { src: html, name: "HTML" },
-        css: { src: css, name: "CSS" },
-        js: { src: js, name: "JavaScript" },
-        android_studio: { src: android_studio, name: "Android Studio" },
-        django: { src: django, name: "Django" },
-        jupyter: { src: jupyter, name: "Jupyter" },
-        streamlit: { src: streamlit, name: "Streamlit" },
-        tableau: { src: tableau, name: "Tableau" },
-        pytorch: { src: pytorch, name: "Pytorch" },
-        postgres: { src: postgres, name: "Postgres" },
-        quasar: { src: quasar, name: "Quasar" },
-        figma: { src: figma, name: "Figma" },
-        qgis: { src: qgis, name: "QGIS" },
-        git: { src: git, name: "Git" },
-        github: { src: github, name: "Github" },
-        docker_logo: { src: docker_logo, name: "Docker" },
-        postman_logo: { src: postman_logo, name: "Postman" },
-        tailwind_logo: { src: tailwind_logo, name: "Tailwind CSS" },
-    };
-    
-    return (
-        <>
-            <div className='grid grid-flow-col grid-rows-6 gap-6 bg-purple-200 bg-opacity-40'>
-            {Object.keys(images).map((key, index) => {
-                    const { src, name } = images[key];
-                    return <StackItem key={key} image={src} ItemName={name} />;
-                })}
-            </div>
-        </>
-    );
+	const images = {
+		react_logo: { src: react_logo, name: "React" },
+		python_logo: { src: python_logo, name: "Python" },
+		html: { src: html, name: "HTML" },
+		css: { src: css, name: "CSS" },
+		js: { src: js, name: "JavaScript" },
+		android_studio: { src: android_studio, name: "Android Studio" },
+		django: { src: django, name: "Django" },
+		jupyter: { src: jupyter, name: "Jupyter" },
+		streamlit: { src: streamlit, name: "Streamlit" },
+		tableau: { src: tableau, name: "Tableau" },
+		pytorch: { src: pytorch, name: "Pytorch" },
+		postgres: { src: postgres, name: "Postgres" },
+		quasar: { src: quasar, name: "Quasar" },
+		figma: { src: figma, name: "Figma" },
+		qgis: { src: qgis, name: "QGIS" },
+		git: { src: git, name: "Git" },
+		github: { src: github, name: "Github" },
+		docker_logo: { src: docker_logo, name: "Docker" },
+		postman_logo: { src: postman_logo, name: "Postman" },
+		tailwind_logo: { src: tailwind_logo, name: "Tailwind CSS" },
+	};
+
+	return (
+		<>
+			<div className="grid grid-flow-col grid-rows-7 gap-7 bg-opacity-40 justify-items-center items-center">
+				{Object.keys(images).map((key, index) => {
+					const { src, name } = images[key];
+					return <StackItem key={index} image={src} ItemName={name} />;
+				})}
+			</div>
+		</>
+	);
 };
 
 export default AboutStack;
--- src/Stack_/StackItem.js
+++ src/Stack_/StackItem.js
@@ -1,19 +1,22 @@
 // src/about/Stack.js
-import React from 'react';
+import React from "react";
 
-const StackItem = ({ItemName, image}) => {
-    return (
-        <div className="text-lg flex flex-row justify-around mx-auto my-auto w-100 bg-opacity-70 p-2 rounded-3">
-            {/* <div className='mt-2'>
-                <h3>{ItemName}</h3>
-            </div> */}
-            <div className=''>
-                <img 
-                    src={image} alt={ItemName} 
-                    className="h-12 w-12" />
-            </div>
-        </div>
-    );
+const StackItem = ({ ItemName, image }) => {
+	return (
+		<div className="block md:flex md:flex-row bg-indigo-300  bg-opacity-70 rounded-3 w-2/3">
+			<div className="my-auto mx-auto h-4/5 w-4/5 md:basis:3xs w-1/3">
+				<img
+					src={image}
+					alt={ItemName}
+					className="mx-auto my-auto h-3/5 w-3/5"
+				/>
+			</div>
+			<div className="md:mr-2 md:content-center mx-auto mt-5 md:basis-xl w-2/3">
+				<h3 className="text-xl md:text-md font-bold">{ItemName}</h3>
+				{/* <h3>Item category</h3> */}
+			</div>
+		</div>
+	);
 };
 
 export default StackItem;
