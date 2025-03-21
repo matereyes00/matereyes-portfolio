c87acb38ba7b615787ae65c92b6cd2ae94cb280b
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2025-03-21 22:33:30 +0800

    Added icons instead of svgs.
    Updated projectcard to make it look neat. returned stakcitem to how it was.

--- src/Projects_/ProjectCard.js
+++ src/Projects_/ProjectCard.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import github from "../images/github.svg";
-import StackItem from '../Stack_/StackItem';
+import StackItem from "../Stack_/StackItem";
 
+const ProjectCard = ({
+	projectTitle,
+	projectDescription,
+	projectLink,
+	projectStack,
+}) => {
+	return (
+		<>
+			<div className="border-2 border-slate-900 rounded-xl p-1">
+				<div
+					className="flex flex-row justify-center mt-3 border-b-2 border-slate-900 
+                w-2/3 mx-auto">
+					<h1 className="text-3xl">{projectTitle}</h1>
+					<FontAwesomeIcon
+						icon={faExternalLink}
+						size="xs"
+						style={{ color: "black" }}
+						className="ml-2"
+					/>
+				</div>
 
-const ProjectCard = ({ projectTitle, projectDescription, projectLink, projectStack }) => {
-    return (
-        <>
-            <div className='border-2 border-slate-900 rounded-xl p-1'>
-                <div className='flex flex-row justify-center mt-3 border-b-2 border-slate-900 
-                w-2/3 mx-auto'>
-                    <h1 className='text-3xl'>{projectTitle}</h1>
-                    <FontAwesomeIcon icon={faExternalLink} size="xs" style={{ color: 'black' }} className='ml-2' />
-                    
-                </div>
-                <div className="flex flex-row justify-center mx-auto w-2/3">
-                    {projectStack.map( (stackItem, index) => (
-                        <div className="w-1/5">
-                            <p key={index}>
-                                <StackItem image={stackItem}/>
-                            </p>
-                        </div>
-                    ))}
-                </div>
-                <p>{projectDescription}</p>
-                <a href={projectLink} target="_blank"><img src={github} alt="github-icon" className='w-14 h-14 mx-auto' /></a>
-            </div>
-        </>
-    );
+				<div className="flex flex-wrap flex-row justify-around w-2/3 flex-auto my-3 mx-auto">
+					{projectStack.map((stackItem, index) => (
+						<div key={index} className="">
+							{stackItem}
+						</div>
+					))}
+				</div>
+
+				<p>{projectDescription}</p>
+
+				<a href={projectLink} target="_blank">
+					<img src={github} alt="github-icon" className="w-10 h-10 mx-auto" />
+				</a>
+			</div>
+		</>
+	);
 };
 
 export default ProjectCard;
--- src/Stack_/StackItem.js
+++ src/Stack_/StackItem.js
@@ -1,17 +1,10 @@
 // src/about/Stack.js
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const StackItem = ({ ItemName, image }) => {
 	return (
-		<div className="block md:flex md:flex-row rounded-3 w-2/3">
-			<div className="my-auto mx-auto h-4/5 w-4/5 md:basis:3xs w-1/3">
-				<img
-					src={image}
-					alt={ItemName}
-					className="mx-auto my-auto h-3/5 w-3/5"
-				/>
-			</div>
-		</div>
+		<img src={image} alt={ItemName} className="mx-auto my-auto w-1/3 h-1/3" />
 	);
 };
 
