40605eab96c4cf95256d8228574230ed5dddc3c9
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2025-04-26 18:30:15 +0800

    Updated publications and presentations portion of the site.

--- src/Home_/Home.jsx
+++ src/Home_/Home.jsx
@@ -13,17 +13,25 @@ function Home() {
 		"Using K-means clustering: performing site selection to identify build-able areas for health centers (RHUs)",
 		"Rust Programming: A Safe, Fast, and Versatile Choice",
 		"Back Propagation and Recurrent Neural Networks for Optimal Location of Rural Health Unit Facilities in the Philippines",
+		"I-HOPE: DEVELOPMENT OF AN INTELLIGENT HEALTH FACILITY OPTIMIZER AND RECOMMENDER SYSTEM FOR OPTIMAL PLACEMENT OF RURAL HEALTH UNITS IN A REGION",
 	];
-	const pub_date = ["December 2, 2023", "December 2, 2023", "August 16, 2024"];
+	const pub_date = [
+		"December 2, 2023",
+		"December 2, 2023",
+		"August 16, 2024",
+		"April 26, 2025",
+	];
 	const pub_authors = [
 		"Martina Therese R. Reyes",
 		"Martina Therese R. Reyes",
+		"Martina Therese R. Reyes, Dr. Ma. Regina Justina E. Estuar, Ph. D., Jann Railey E. Montalan, M.S.",
 		"Martina Therese R. Reyes, Dr. Ma. Regina Justina E. Estuar, Ph. D., Jann Railey E. Montalan, M.S.",
 	];
 	const pub_article_urls = [
 		"https://medium.com/@martina.therese.reyes/using-k-means-clustering-performing-site-selection-to-identify-build-able-areas-for-health-centers-a089ef18f86f",
 		"https://medium.com/@martina.therese.reyes/rust-programming-a-safe-fast-and-versatile-choice-71428b87c972",
 		"https://drive.google.com/file/d/10-Qm1QeDph4nSiPx-jGJ_S06IftOo0Va/view?usp=sharing",
+		"https://drive.google.com/file/d/1DK1gzMw3peN7kZDOd-UGfFbfjEvFqNrv/view",
 	];
 	const pub_descriptions = [
 		//first
@@ -32,6 +40,8 @@ function Home() {
 		"This article was the final output of our Programming Languages and Paradigms class for my M.S. Computer Science course (CSCI 202). The task was to choose one programming language and research about it. I chose the Rust programming language because I personally never used it in the academic nor professional setting. The article contains a compact yet detailed discussion of Rust, its unique features,advantages, disadvantages, and usecases in the industry.",
 		//third
 		"The 10th International Conference on Public Health - ICOPH 2024 was held in Bangkok, Thailand on the 15-16th of August 2024. This conference gave me the platform to share my research by presenting in a public forum. I showcased my work on optimizing health facility locations in the Philippines using neural networks. Though it was mainly a proof of concept, it can adapt and be flexible depending on the location it will be applied on. I was very proud to answer during the Q&A portion because the audience and panel seemed interested in the study. Overall, it was an engaging experience because I was able to listen to other students, health experts and/or policy makers and doctors (to name a few!) present their work on how they solve public health issues in their country.",
+		// fourth
+		"The 7th Graduate Research Festival is a celebration of all graduate students who present their completed their masteral or doctorate studies.",
 	];
 	const embeds = [
 		<></>,
@@ -42,7 +52,8 @@ function Home() {
 				class="w-full aspect-video"
 				allow="autoplay"
 				className="m-auto"
-				title="ICOPH: presentation"></iframe>
+				title="ICOPH: presentation"
+			></iframe>
 		</>,
 	];
 
@@ -64,7 +75,8 @@ function Home() {
 		<>
 			<div
 				id="home-terminal"
-				className="text-purple-950 w-4/5 my-8 mx-auto rounded-md">
+				className="text-purple-950 w-4/5 my-8 mx-auto rounded-md"
+			>
 				<div id="home-terminal-content" className="flex flex-col w-full">
 					<WhoAmI
 						Name={who_am_i_headline}
