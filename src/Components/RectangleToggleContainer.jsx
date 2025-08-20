112a3c43eadbb69772164061871508debb8da851
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2025-08-21 00:16:05 +0800

    Added toggle effect to the about page for each section. (as per the react dev roadmap)

--- src/Components/RectangleToggleContainer.jsx
+++ src/Components/RectangleToggleContainer.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import {
+	faArrowCircleDown,
+	faArrowDown,
+	faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 
 const RectangleContainerToggle = (props) => {
+	const [isVisible, setIsVisible] = useState(false);
+	const showDescription = () => {
+		setIsVisible(!isVisible);
+	};
+
 	return (
 		<>
-			<div>
-				<p>container paragraph</p>
+			<div className={props.containerStyle}>
+				<h1 className="text-5xl">{props.title}</h1>
+				{isVisible && (
+					<div className={props.imageStyle}>
+						<div>{props.images}</div>
+						{props.imagesOrigLayout}
+						{props.content}
+					</div>
+				)}
+				<button onClick={showDescription}>
+					{isVisible ? (
+						<FontAwesomeIcon
+							icon={faArrowUp}
+							size="2x"
+							style={{ color: "black" }}
+						/>
+					) : (
+						<FontAwesomeIcon
+							icon={faArrowDown}
+							size="2x"
+							style={{ color: "black" }}
+						/>
+					)}
+				</button>
 			</div>
 		</>
 	);
--- src/features/About/Journey.jsx
+++ src/features/About/Journey.jsx
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faTrain } from "@fortawesome/free-solid-svg-icons";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import RectangleContainerToggle from "../../Components/RectangleToggleContainer";
 
 const EventText = () => {
 	const content = (
@@ -80,102 +81,127 @@ const EventText = () => {
 		</p>
 	);
 
+	const content6 = (
+		<p className="my-auto md:text-left">
+			In my third year, I was{" "}
+			<a
+				href="https://feature-about-page-ebac-22--compsat.netlify.app/about"
+				className="underline"
+			>
+				Secretary-General of CompSAt
+			</a>
+			. I worked alongside different departments that led the organization. My
+			main tasks were internal-based: to document what was happening in the org
+			for the other departments. I was given the opportunity to lead a project
+			that revived an old newsletter of the organization: Compress. The team was
+			made of designers and writers and led by the Documentations and Creatives
+			Department of the org. The aim was to showcase what the organization was
+			all about, given the social-distancing aspect.
+		</p>
+	);
+
 	return (
 		<>
-			<div className="md:flex md:flex-wrap mx-10 my-10  text-blue-950 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl">
-				<div className="hidden md:flex md:flex-wrap md:flex-row justify-center">
-					<img
-						src={poveda_grad}
-						alt="Poveda Graduation 2019"
-						className="w-1/5 mx-auto my-2 md:mx-4"
-					/>
-					<img
-						src={bs_admu_grad}
-						alt="Ateneo Graduation 2023"
-						className="w-1/5 mx-auto my-2 md:mx-4"
-					/>
-				</div>
-				<img
-					src={poveda_grad}
-					alt="Poveda Graduation 2019"
-					className="h-1/2 w-1/2 mx-auto my-2 md:hidden"
-				/>
-				<p className="">{content}</p>
-				<img
-					src={bs_admu_grad}
-					alt="Ateneo Graduation 2023"
-					className="h-1/2 w-1/2 mx-auto my-2 md:hidden"
-				/>
-			</div>
+			<RectangleContainerToggle
+				title="Introduction"
+				content={content}
+				images={
+					<>
+						<img
+							src={poveda_grad}
+							alt="Poveda Graduation 2019"
+							className="w-1/5 mx-auto my-2 md:mx-2"
+						/>
+						<img
+							src={bs_admu_grad}
+							alt="Ateneo Graduation 2023"
+							className="w-1/5 mx-auto my-2 md:mx-2"
+						/>
+					</>
+				}
+				containerStyle="md:flex md:flex-wrap flex-col items-center mx-10 my-10 text-blue-950 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl"
+			/>
+
 			<FontAwesomeIcon icon={faTrain} size="3x" style={{ color: "black" }} />
 
-			<div className="md:flex md:flex-row mx-10 my-10 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl">
-				<img
-					src={compsat_workshop}
-					alt="CompSAt Git Workshop 2019"
-					className="h-1/3 w-1/3 mx-auto my-2"
-				/>
-				<p className="my-auto text-center md:text-right">{content2}</p>
-			</div>
+			<RectangleContainerToggle
+				title="Discovery"
+				content={content2}
+				images={
+					<>
+						<img
+							src={compsat_workshop}
+							alt="CompSAt Git Workshop 2019"
+							className="h-1/5 w-1/5 mx-auto my-2"
+						/>
+					</>
+				}
+				containerStyle="md:flex flex-col items-center mx-10 my-10 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl"
+			/>
+
 			<FontAwesomeIcon
 				icon={faLocationDot}
 				size="3x"
 				style={{ color: "black" }}
 			/>
 
-			<div className="md:flex md:flex-row mx-10 my-10  text-blue-950 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl">
-				<p className="my-auto md:text-left">
-					In my third year, I was{" "}
-					<a
-						href="https://feature-about-page-ebac-22--compsat.netlify.app/about"
-						className="underline"
-					>
-						Secretary-General of CompSAt
-					</a>
-					. I worked alongside different departments that led the organization.
-					My main tasks were internal-based: to document what was happening in
-					the org for the other departments. I was given the opportunity to lead
-					a project that revived an old newsletter of the organization:
-					Compress. The team was made of designers and writers and led by the
-					Documentations and Creatives Department of the org. The aim was to
-					showcase what the organization was all about, given the
-					social-distancing aspect.
-				</p>
-				<img
-					src={compsat_dp}
-					alt="CompSAt 2021 FB DP"
-					className="mx-auto w-1/2 h-1/2 my-2 md:mx-4"
-				/>
-			</div>
+			<RectangleContainerToggle
+				title="Org life"
+				content={content6}
+				images={
+					<img
+						src={compsat_dp}
+						alt="CompSAt 2021 FB DP"
+						className="mx-auto w-1/5 h-1/5 my-2 md:mx-4"
+					/>
+				}
+				containerStyle="md:flex flex-col items-center mx-10 my-10  text-blue-950 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl"
+			/>
+
 			<FontAwesomeIcon icon={faCoffee} size="3x" style={{ color: "black" }} />
 
-			<div className="flex flex-row mx-10 my-10 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl">
-				<p className="my-auto">{content3}</p>
-			</div>
+			<RectangleContainerToggle
+				title="Side quests"
+				content={content3}
+				containerStyle="flex flex-col items-center mx-10 my-10 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl"
+			/>
+
 			<FontAwesomeIcon icon={faTrain} size="3x" style={{ color: "black" }} />
 
-			<div className="md:flex md:flex-row mx-10 my-10  text-blue-950 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl">
-				<p className="my-auto text-center md:text-left">{content4}</p>
-				<img
-					src={ico_ph}
-					alt="ICOPH 2024 Conference"
-					className="h-1/2 w-1/2 mx-auto my-2"
-				/>
-			</div>
+			<RectangleContainerToggle
+				title="Student becomes the Master"
+				images={
+					<>
+						<img
+							src={ico_ph}
+							alt="ICOPH 2024 Conference"
+							className="h-1/5 w-1/5 mx-auto my-2"
+						/>
+					</>
+				}
+				content={content4}
+				paragraphStyle="my-auto text-center md:text-left"
+				containerStyle="md:flex flex-col items-center mx-10 my-10  text-blue-950 bg-opacity-60 p-5 border-2 border-slate-900 rounded-xl"
+			/>
+
 			<FontAwesomeIcon
 				icon={faLocationDot}
 				size="3x"
 				style={{ color: "black" }}
 			/>
 
-			<div className="mx-10 my-10 bg-opacity-60 p-5 md:flex md:flex-row  border-2 border-slate-900 rounded-xl">
-				<img
-					src={ubx_ps}
-					alt="UBX Photoshoot"
-					className="h-1/3 w-1/3 mx-auto"
-				/>
-				<p className="my-auto text-center md:text-right ">{content5}</p>
-			</div>
+			<RectangleContainerToggle
+				title="Where is she now?"
+				images={
+					<img
+						src={ubx_ps}
+						alt="UBX Photoshoot"
+						className="h-1/3 w-1/3 mx-auto"
+					/>
+				}
+				content={content5}
+				containerStyle="mx-10 my-10 bg-opacity-60 p-5 md:flex md:flex-col items-center border-2 border-slate-900 rounded-xl"
+			/>
 		</>
 	);
 };
